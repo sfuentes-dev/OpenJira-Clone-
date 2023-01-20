578a7d8affa096857605eff66d7a3ba1cbafcee4
sfuentes-dev <sfuentes.dev@gmail.com>
2023-01-20 17:45:28 -0500

    Secction 7: Finalizada

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import { UIContext } from '../../context/ui/UIContext';
+
+export const Navbar = () => {
+  const { openSideMenu } = useContext(UIContext);
+
+  return (
+    <AppBar position='sticky'>
+      <Toolbar>
+        <IconButton size='large' edge='start' onClick={openSideMenu}>
+          <MenuOutlinedIcon />
+        </IconButton>
+        <h1></h1>
+        <Typography variant='h6'>OpenJira</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { lightTheme, darkTheme } from '../themes';
+import { UIProvider } from '@/context/ui';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
+  );
 }
